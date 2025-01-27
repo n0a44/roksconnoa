04062eede8a7dc4ece46e468158e3193f5b30af7
n0a44 <noadragoi@gmail.com>
2025-01-27 18:32:21 +0200

    fix player n camera

--- ROKSYNOA/Assets/CameraRotation.cs
+++ ROKSYNOA/Assets/CameraRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+	public GameObject player;
 	public float Sensitivity
 	{
 		get { return sensitivity; }
@@ -19,12 +20,16 @@ public class CameraRotation : MonoBehaviour
 
 	void Update()
 	{
+		Vector3 poz ;
+		poz = new Vector3(0, 0.6f, 0);
 		rotation.x += Input.GetAxis(xAxis) * sensitivity;
 		rotation.y += Input.GetAxis(yAxis) * sensitivity;
 		rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
 		var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
 		var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
 
-		transform.localRotation = xQuat * yQuat; 
+		transform.localRotation = xQuat * yQuat;
+		transform.position = player.transform.position+poz;
+		
 	}
 }
--- ROKSYNOA/Assets/PlayerMovement.cs
+++ ROKSYNOA/Assets/PlayerMovement.cs
@@ -6,34 +6,56 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public GameObject player;
-   
-    
-    void Start()
+    private float orizontal;
+    private float vertical;
+
+    public float Sensitivity
     {
-        
+        get { return sensitivity; }
+        set { sensitivity = value; }
     }
+    [Range(0.1f, 9f)] [SerializeField] float sensitivity = 2f;
+
+    [Range(0f, 90f)] [SerializeField] float yRotationLimit = 88f;
+
+    Vector2 rotation = Vector2.zero;
+    const string xAxis = "Mouse X";
+    const string yAxis = "Mouse Y";
 
     // Update is called once per frame
     void Update()
     {
        
-        if(Input.GetKey(KeyCode.W))
-        {
-            player.transform.position += Vector3.forward * speed * Time.deltaTime;
-            Debug.Log("blabla");
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            player.transform.position += Vector3.back  * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            player.transform.position += Vector3.left * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            player.transform.position += Vector3.right * speed * Time.deltaTime;
-        }
+        orizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector3 poz;
+        poz = new Vector3(0, 0.6f, 0);
+        rotation.x += Input.GetAxis(xAxis) * sensitivity;
+        rotation.y += Input.GetAxis(yAxis) * sensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+        var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+        transform.localRotation = xQuat * yQuat;
+        
+        
+         if(Input.GetKey(KeyCode.W))
+         {
+             player.transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
+
+         }
+         if(Input.GetKey(KeyCode.S))
+         {
+             player.transform.position += transform.TransformDirection(Vector3.back) * speed * Time.deltaTime;
+         }
+         if (Input.GetKey(KeyCode.A))
+         {
+             player.transform.position += transform.TransformDirection(Vector3.left) * speed * Time.deltaTime;
+         }
+         if (Input.GetKey(KeyCode.D))
+         {
+             player.transform.position += transform.TransformDirection(Vector3.right) * speed * Time.deltaTime;
+         }
     }
 
 }
